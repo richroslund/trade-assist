ae907cb503eef75d6485a34f94399dae9889bda5
= <=>
2018-03-17 03:05:24 -0500

    Adding socket.io to server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
+var http = require('http')
+const server = http.createServer(app)
 var {getPrice, getBalance, trades, getAllSymbols, stats} = require('../trader/dist/trader');
+
+var io = require('socket.io')(server);
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
@@ -11,6 +15,17 @@ app.get('/balance/:coin', async (req, res, coin) => {
   
 });
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+server.listen(3030, function() {
+  console.log('Example app listening on port 3030!');
+});
+
+io.on('connection', function (socket) {
+  console.log("new client connected!");
+  setInterval(
+    () => getPrice("eth").then((p) => {
+      console.log(p);
+      socket.emit("price update", p)
+    }),
+    5000
+  );
 });
